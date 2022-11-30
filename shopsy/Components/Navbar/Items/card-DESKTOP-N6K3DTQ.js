4df9740d307e8d3bffdd5648cdc651a38b2212d3
RICHARD STEPHEN - STUDENT <G00382880@atu.ie>
2022-11-30 12:41:12 +0000

    Turned Items into buttons

--- shopsy/Components/Navbar/Items/card-DESKTOP-N6K3DTQ.js
+++ shopsy/Components/Navbar/Items/card-DESKTOP-N6K3DTQ.js
@@ -4,71 +4,53 @@ import style from "./item.module.css";
 export default function Card() {
   return (
     <div className={style.container}>
-
-     
+      
       <div className={style.item}>
-        <button className={style.button}>
         <Item
           title="Lebron james"
           image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
-          price="15,000"
+          price="$15,000"
         />
-        </button>
-      </div>
- 
-
-      <div className={style.item}>
-      <button className={style.button}>
-        <Item
-          title="John Cena (Forma Bing Chilling)"
-          image="https://e.snmc.io/i/300/s/ae17e2c156474befef2a237ac4f45940/9543622"
-          price="15,000"
-        />
-        </button>
       </div>
 
       <div className={style.item}>
-        <button className={style.button}>
         <Item
-          title="Lebron james"
-          image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
-          price="15,000"
+          title="John Cena (Forma Bing chilling)"
+          image="https://pbs.twimg.com/ext_tw_video_thumb/1458075877884022787/pu/img/EdV3z0oi3k3bzVrd.jpg:large"
+          price="$150,000"
         />
-        </button>
       </div>
- 
 
       <div className={style.item}>
-        <button className={style.button}>
         <Item
-          title="Lebron james"
-          image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
-          price="15,000"
+          title="The Rock (Forma La Eyebrow)"
+          image="https://external-preview.redd.it/iSpSIg6Vu4paQxA77pKwfwMcOx9iRkVr8tIUVvsjsBs.png?format=pjpg&auto=webp&s=7e23a2aacc1f3e583c42080b0daa03438290eeae"
+          price="$2,000,000"
         />
-        </button>
       </div>
- 
-      <div className={style.item}>
-        <button className={style.button}>
-        <Item
-          title="Lebron james"
-          image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
-          price="15,000"
-        />
-        </button>
-      </div>
- 
 
       <div className={style.item}>
-        <button className={style.button}>
+        <Item
+          title="Dababy (Forma la coche)"
+          image="https://i.kym-cdn.com/entries/icons/facebook/000/036/822/cover4.jpg"
+          price="$1,000"
+        />
+      </div>
+      <div className={style.item}>
         <Item
           title="Lebron james"
           image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
-          price="15,000"
+          price="$15,000"
         />
-        </button>
       </div>
- 
+
+      <div className={style.item}>
+        <Item
+          title="Lebron james"
+          image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"
+          price="$15,000"
+        />
+      </div>
     </div>
   );
 }
--- shopsy/Components/Navbar/Items/item.js
+++ shopsy/Components/Navbar/Items/item.js
@@ -2,10 +2,9 @@ import style from "./item.module.css"
 export default function Item(props){
     return(
         <div className={style.detail}>
-        <img src = {props.image}/>
-        <h1> {props.title}</h1>
+        <img src = {props.image} className={style.imgProp}/>
+        <h1 className={style.title}> {props.title} </h1>
         <h3> {props.price}</h3>
-
         </div>
     )
 }
