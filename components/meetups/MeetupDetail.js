2399eea3f08c5189e1cdb91679607ccd5eaf2112
RICHARD STEPHEN - STUDENT <G00382880@atu.ie>
2022-12-12 19:08:51 +0000

    Added border to product description

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -6,6 +6,7 @@ function MeetupDetail(props) {
             <img src={props.image} alt={props.title} />
             <div className={classes.description}>
             <h1>{props.title}</h1>
+            <h1>Product Description:</h1>
             <p>{props.description}</p>
             <p>Price: $ {props.price}</p>
             </div>
