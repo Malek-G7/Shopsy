710716d6da74b9f018d38a2430358df806282895
MALEK GESHASH - STUDENT <G00387032@atu.ie>
2022-12-13 12:58:19 +0000

    work in progress

---
+++ components/meetups/BasketList.js
@@ -0,0 +1,26 @@
+import BasketItem from './BasketItem';
+import classes from './MeetupList.module.css';
+
+function BasketList(props) {
+  let quantity = new Array(props.meetups.length) 
+  quantity.fill(0)
+  return (
+    <div className={classes.container}>
+      {props.meetups.map((meetup) => (
+        <div className={classes.item}>
+        <BasketItem
+          key={meetup.meetingId}
+          id={meetup.meetingId}
+          image={meetup.image}
+          title={meetup.title}
+          price={meetup.price}
+        />
+        </div>
+      ))}
+
+    </div>
+
+  );
+}
+
+export default BasketList;
--- pages/basket.js
+++ pages/basket.js
@@ -1,4 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList'
+import BasketList from '../components/meetups/BasketList'
 import { useContext,useEffect } from "react";
 import GlobalContext from "./store/globalContext"
 import { useRouter } from 'next/router';
@@ -9,7 +10,6 @@ function HomePage() {
     const router = useRouter()
     let total = 0
     let basket = new Array(globalCtx.theGlobalObject.meetings.length)
-    
     for (let ii = 0; ii < globalCtx.theGlobalObject.meetings.length; ii++) {
         if (globalCtx.theGlobalObject.basket[ii] == true ) {
             basket.push(globalCtx.theGlobalObject.meetings[ii])
@@ -26,7 +26,7 @@ function HomePage() {
   
     return(
     <div>
-        <MeetupList meetups={basket} /> 
+        <BasketList meetups={basket} /> 
          <button className = {classes.button}  onClick = {()=> {globalCtx.checkout(); router.push('/')}}  >checkout</button>
          <div className={classes.total}>
          total : $ {totalPrice()} 
